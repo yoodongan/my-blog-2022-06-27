d4a638b43e2d36cd5255b13680dead9be3c8abf2
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-06-28 17:39:47 +0900

    오류 발생

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,27 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 function BlogPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+        }
+      }
+    }
+  `);
+
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
+
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
