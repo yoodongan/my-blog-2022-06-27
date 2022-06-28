f167f0a5afaa66db0becb4ee44165b9d39bc32b7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-06-28 21:49:05 +0900

    오류 수정, layout -> Layout

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,10 +1,24 @@
 import React from "react";
-import Layout from "../../components/layout";
-const BlogPost = () => {
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../../components/Layout";
+const BlogPost = ({ data }) => {
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
 export default BlogPost;
