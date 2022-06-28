9eb044e6034512db8af01e9daaaed9811d1f8314
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-06-28 17:20:15 +0900

    blog 디렉토리에 mdx 파일들 생성, graphql로 읽기

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+function BlogPage() {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My cool posts will go in here</p>
+    </Layout>
+  );
+}
+
+export default BlogPage;
